cc68455e82bad424cd63ab06a77d1038144523fc
456789z <61338510+456789z@users.noreply.github.com>
2020-07-01 14:46:40 +0630

    Create fbhack.py

---
+++ fbhack.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import 
+os.system("clear")
+os.system("apt install figlet")
+os.system("figlet Bruteforce")
+a = input("id/email>>")
+a = input("pasword>>")
+os.system("clear")
+print ("hacking starting...10 ")
+os.system("sleep 3")
+print ("hacking starting...20 ")
+os.system("sleep 3")
+print ("hacking starting...30 ")
+os.system("sleep 3")
+print ("hacking starting...40 ")
+os.system("sleep 3")
+print ("hacking starting...50 ")
+os.system("sleep 3")
+print ("hacking starting...80 ")
+print ("hacking starting...90 ")
+print ("Hack Done... ")
+os.system("sleep 3")
+print ("===================")
+print ("!password>>minmaylol!")
+print ("!===================")
+
